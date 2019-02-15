bf285b9487f13ff71501d4fd7f7f7418591f766f
nagerz <znager@gmail.com>
2019-02-15 10:42:51 -0700

    Added model test for average price

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -6,6 +6,19 @@ describe Machine, type: :model do
     it { should belong_to :owner }
     it { should have_many :machine_snacks }
     it { should have_many(:snacks).through :machine_snacks }
-
   end
+
+  describe 'instance methods' do
+    it 'can calculate average snack price' do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+      Snack.create!(machine_id: dons.id, name: "White Castle Burger", price: 3.5)
+      Snack.create(machine_id: dons.id, name: "Pop Rocks", price: 1.5)
+      Snack.create(machine_id: dons.id, name: "Flaming Hot Cheetos", price: 2.5)
+
+      expect(dons.average_price). to eq(2.5)
+    end
+  end
+
 end
