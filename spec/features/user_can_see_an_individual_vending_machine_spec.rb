290952c87a1e47999725be7d94ff9a1aac1c2f0b
nagerz <znager@gmail.com>
2019-02-15 10:22:44 -0700

    Added snack and machinesnakcs migrations and models

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,23 @@ feature 'When a user visits a vending machine show page' do
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  scenario 'they see the name of all snacks with a price' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    snack_1 = Snack.create!(name: "White Castle Burger", price: 3.5)
+    snack_2 = Snack.create(name: "Pop Rocks", price: 1.5)
+    snack_3 = Snack.create(name: "Flaming Hot Cheetos", price: 2.5)
+    snacks = [snack_1, snack_2, snack_3]
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Snacks:")
+
+    snacks.each do |snack|
+      expect(page).to have_content("#{snack.name}: #{snack.cost}")
+    end
+
+  end
 end
